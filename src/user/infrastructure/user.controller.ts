4455147269a4eaec5108f7e2a20eb51282f2ad29
andres06-hub <andressierrarojas7@gmail.com>
2023-03-13 19:48:21 -0500

    ADD: implementation, injection of the port(interface)

--- src/user/infrastructure/user.controller.ts
+++ src/user/infrastructure/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { UserServiceInterface } from '../application/user.interface';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    @Inject('UserServiceInterface')
+    private readonly _userSrv: UserServiceInterface,
+  ) {}
+
+  @Get()
+  async getUser(id: string) {
+    return this._userSrv.getUser(id);
+  }
+}
