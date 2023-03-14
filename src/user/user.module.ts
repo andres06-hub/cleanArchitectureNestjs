4b7da3c60eeabcc6eb0c3300bf4f5f5aa030ae4c
andres06-hub <andressierrarojas7@gmail.com>
2023-03-13 19:50:03 -0500

    ADD: asigned provider with token, use class UserService

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './infrastructure/user.controller';
+import { UserService } from './application/user.service';
+import { UserServiceInterface } from './application/user.interface';
 
-@Module({})
+@Module({
+  imports: [],
+  controllers: [UserController],
+  providers: [
+    {
+      provide: 'UserServiceInterface',
+      useClass: UserService,
+    },
+  ],
+  exports: [],
+})
 export class UserModule {}
