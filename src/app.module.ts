42149753c4a4e0b6fc0953599436c5275321afa2
andres06-hub <andressierrarojas7@gmail.com>
2023-03-13 23:02:43 -0500

    ADD: provider module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProvidersModule } from './providers/providers.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProvidersModule],
+  imports: [ProvidersModule, UserModule],
   controllers: [],
   providers: [],
 })
