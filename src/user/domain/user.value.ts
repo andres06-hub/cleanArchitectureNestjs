70109e774e833af45a9b9133a73648a00243c284
andres06-hub <andressierrarojas7@gmail.com>
2023-03-22 18:59:27 -0500

    ADD: attribute - id

--- src/user/domain/user.value.ts
+++ src/user/domain/user.value.ts
@@ -1,19 +1,23 @@
-import { UserEntity } from '@user/domain/interfaces/user.entity';
+import { UserModel } from '@src/user/domain/models/user.model';
 
-export class user implements UserEntity {
+export class UserValue implements UserModel {
+  id: number;
   name: string;
   email: string;
   password: string;
 
   constructor({
+    id,
     name,
     email,
     password,
   }: {
+    id: number;
     name: string;
     email: string;
     password: string;
   }) {
+    this.id = id;
     this.name = name;
     this.email = email;
     this.password = password;
