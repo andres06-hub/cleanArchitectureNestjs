bc514005fe90537bd7c08e27c97450e37a71230f
andres06-hub <andressierrarojas7@gmail.com>
2023-03-22 18:56:17 -0500

    ADD: Connection log

--- src/providers/database/db.provider.ts
+++ src/providers/database/db.provider.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
-import { User } from '@user/infrastructure/models/user.model';
+import { User } from '@src/user/infrastructure/entities/user.entity';
 import { Logger } from '@nestjs/common';
 
 config({ path: 'db.env' });
@@ -18,7 +18,7 @@ export const databaseProviders = [
         synchronize: true,
       });
       const x = await dataSource.initialize();
-      logger.log('Connection DB');
+      logger.log('Connection DataBase');
       return x;
     },
   },
