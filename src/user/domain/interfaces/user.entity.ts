9d49e39f5bb295c36fa139ba8245df7b060869f6
andres06-hub <andressierrarojas7@gmail.com>
2023-03-22 18:57:00 -0500

    CHANGE: name

--- src/user/domain/interfaces/user.entity.ts
+++
@@ -1,5 +0,0 @@
-export interface UserEntity {
-  name: string;
-  email: string;
-  password: string;
-}
--- src/user/domain/interfaces/user.repository.ts
+++
@@ -1,5 +0,0 @@
-export const USER_REPOSITORY = 'USER REPOSITORY';
-
-export interface UserRepository {
-  findUserById(id: string): Promise<string>;
-}
