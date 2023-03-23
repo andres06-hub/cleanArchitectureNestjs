7488c1409f954ed364d36997c92b0c6a30dc1f41
andres06-hub <andressierrarojas7@gmail.com>
2023-03-22 19:00:32 -0500

    ADD: attribute - function

--- src/user/application/ports/user.interface.ts
+++ src/user/application/ports/user.interface.ts
@@ -1,5 +1,8 @@
+import { UserValue } from '@src/user/domain/user.value';
+
 export const USER_SERVICE_INTERFACE = 'UserServiceInterface';
 
 export interface UserServiceInterface {
-  findUserById(id: string);
+  findUserById(id: string): Promise<string>;
+  createUser(user: UserValue): Promise<UserValue>;
 }
