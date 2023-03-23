08c82b14c4b183d7dfffd094192023eb8264b2e7
andres06-hub <andressierrarojas7@gmail.com>
2023-03-22 19:04:32 -0500

    ADD: provider

--- src/user/infrastructure/provider/user.provider.ts
+++ src/user/infrastructure/provider/user.provider.ts
@@ -1,5 +1,5 @@
 import { DataSource } from 'typeorm';
-import { User } from '@user/infrastructure/models/user.model';
+import { User } from '@src/user/infrastructure/entities/user.entity';
 import { USER_SERVICE_INTERFACE } from '@user/application/ports/user.interface';
 import { UserService } from '@user/application/user.service';
 
@@ -12,4 +12,9 @@ export const UserDbProvider = {
 export const UserSrvIntProvider = {
   provide: USER_SERVICE_INTERFACE,
   useClass: UserService,
+};
+
+export const UsernetitiesProvider = {
+  provider: 'USER_MODEL',
+  useFactory: () => [User],
 };
