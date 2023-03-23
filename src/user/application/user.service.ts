64d7ffa5d5eb9ee00bf378f1d4c9e7418b149c9f
andres06-hub <andressierrarojas7@gmail.com>
2023-03-22 19:01:32 -0500

    ADD: functionality

--- src/user/application/user.service.ts
+++ src/user/application/user.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { UserRepository } from '@user/domain/interfaces/user.repository';
+import { UserRepository } from '@src/user/domain/repositories/user.repository';
+import { UserValue } from '@user/domain/user.value';
+import { UserServiceInterface } from '@user/application/ports/user.interface';
 
 @Injectable()
-export class UserService implements UserRepository {
+export class UserService implements UserServiceInterface {
+  // constructor(private userReposirory: UserRepository) {}
+
   async findUserById(id: string): Promise<string> {
     console.log('Get User...' + id);
     return `HELLO WORLD id: ${id}`;
+  }
+  async createUser(user: UserValue): Promise<UserValue> {
+    const newUser: UserValue = new UserValue(user);
+    console.log(newUser);
+    return newUser;
   }
 }
