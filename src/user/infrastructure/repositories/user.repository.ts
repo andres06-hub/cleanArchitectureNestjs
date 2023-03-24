d5a277e521fea9fe272bcea32fe55be41078eaff
andres06-hub <andressierrarojas7@gmail.com>
2023-03-23 19:32:02 -0500

    CREATED: repository for database connection

---
+++ src/user/infrastructure/repositories/user.repository.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { UserRepository } from '@src/user/domain/repositories/userRepository.interface';
+import { UserValue } from '@src/user/domain/user.value';
+import { Repository } from 'typeorm';
+import { User } from '@user/infrastructure/entities/user.entity';
+
+@Injectable()
+export class DatabaseUserRepository implements UserRepository {
+  constructor(
+    @Inject('USER_CONNECTION')
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findUserByPublicId(publicId: string): Promise<User | null> {
+    const user: User | null = await this.userRepository.findOne({
+      where: { publicId },
+    });
+    return user;
+  }
+  async createUser(user: UserValue): Promise<UserValue> {
+    // throw new Error('Method not implemented.');
+    const newUser = this.userRepository.create(user);
+    return await this.userRepository.save(newUser);
+  }
+
+  toUserEntiry(data: UserValue): User {
+    const user: User = new User();
+    user.id = data.id;
+    user.publicId = data.publicId;
+    user.name = data.name;
+    user.password = data.password;
+    user.email = data.email;
+    return user;
+  }
+}
