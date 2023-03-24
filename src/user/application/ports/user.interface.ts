b990687bf95675d95d7add572cbd8d206b5ac831
andres06-hub <andressierrarojas7@gmail.com>
2023-03-23 19:26:26 -0500

    CHANGE: return type

--- src/user/application/ports/user.interface.ts
+++ src/user/application/ports/user.interface.ts
@@ -3,6 +3,6 @@ import { UserValue } from '@src/user/domain/user.value';
 export const USER_SERVICE_INTERFACE = 'UserServiceInterface';
 
 export interface UserServiceInterface {
-  findUserById(id: string): Promise<string>;
+  findUserById(id: string): Promise<UserValue>;
   createUser(user: UserValue): Promise<UserValue>;
 }
