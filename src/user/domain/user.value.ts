6f4aa2828bb8a77165ef8df82702d92a04cf6641
andres06-hub <andressierrarojas7@gmail.com>
2023-03-23 19:23:14 -0500

    ADD: attribute

--- src/user/domain/user.value.ts
+++ src/user/domain/user.value.ts
@@ -2,22 +2,26 @@ import { UserModel } from '@src/user/domain/models/user.model';
 
 export class UserValue implements UserModel {
   id: number;
+  publicId: string;
   name: string;
   email: string;
   password: string;
 
   constructor({
     id,
+    publicId,
     name,
     email,
     password,
   }: {
     id: number;
+    publicId: string;
     name: string;
     email: string;
     password: string;
   }) {
     this.id = id;
+    this.publicId = publicId;
     this.name = name;
     this.email = email;
     this.password = password;
