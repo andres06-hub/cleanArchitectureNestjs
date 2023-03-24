9544c070975a9dae538cf9a5c18ce088334cd146
andres06-hub <andressierrarojas7@gmail.com>
2023-03-24 18:54:16 -0500

    ADD: function for find by Email

--- src/user/domain/repositories/userRepository.interface.ts
+++ src/user/domain/repositories/userRepository.interface.ts
@@ -3,5 +3,6 @@ import { UserValue } from '@user/domain/user.value';
 export const USER_REPOSITORY = 'USER REPOSITORY';
 export interface UserRepository {
   findUserByPublicId(publicId: string): Promise<UserValue | null>;
+  findUserByEmail(email: string): Promise<UserValue | null>;
   createUser(user: UserValue): Promise<UserValue>;
 }
