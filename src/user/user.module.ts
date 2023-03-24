2d7c3e8155dd17cbdaaedb79052197e8d2911621
andres06-hub <andressierrarojas7@gmail.com>
2023-03-23 19:33:44 -0500

    ADD: modules

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,13 +3,14 @@ import { UserController } from '@user/infrastructure/controllers/user.controller
 import {
   UserDbProvider,
   UserSrvIntProvider,
-} from '@user/infrastructure/provider/user.provider';
+} from '@src/user/infrastructure/providers/user.provider';
 import { DatabaseModule } from '@providers/database/database.module';
+import { DatabaseUserRepository } from './infrastructure/repositories/user.repository';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserDbProvider, UserSrvIntProvider],
+  providers: [UserDbProvider, DatabaseUserRepository, UserSrvIntProvider],
   exports: [],
 })
 export class UserModule {}
