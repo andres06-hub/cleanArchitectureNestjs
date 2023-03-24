a083348afc86ecf0bf18a60e16bdfbb4a1276afa
andres06-hub <andressierrarojas7@gmail.com>
2023-03-23 19:23:58 -0500

    CHANGE: name

--- src/user/domain/repositories/user.repository.ts
+++
@@ -1,8 +0,0 @@
-import { UserValue } from '@user/domain/user.value';
-
-export const USER_REPOSITORY = 'USER REPOSITORY';
-
-export interface UserRepository {
-  findUserById(id: string): Promise<string>;
-  createUser(user: UserValue): Promise<UserValue>;
-}
