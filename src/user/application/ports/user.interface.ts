4a1b8df2917752ea1a9e8fbcb7d0a2314b058588
andres06-hub <andressierrarojas7@gmail.com>
2023-03-24 18:55:00 -0500

    MODIFIED: function type

--- src/user/application/ports/user.interface.ts
+++ src/user/application/ports/user.interface.ts
@@ -1,8 +1,9 @@
+import { Response } from '@src/user/domain/dto/response.dto';
 import { UserValue } from '@src/user/domain/user.value';
 
 export const USER_SERVICE_INTERFACE = 'UserServiceInterface';
 
 export interface UserServiceInterface {
-  findUserById(id: string): Promise<UserValue>;
-  createUser(user: UserValue): Promise<UserValue>;
+  findUserById(publicId: string): Promise<Response>;
+  createUser(user: UserValue): Promise<Response>;
 }
