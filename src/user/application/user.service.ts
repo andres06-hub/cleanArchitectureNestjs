a00ade29092b70c6cac93770083d4ee412466d77
andres06-hub <andressierrarojas7@gmail.com>
2023-03-23 19:28:05 -0500

    CREATED: inject user repository, create user use cases

--- src/user/application/user.service.ts
+++ src/user/application/user.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { UserRepository } from '@src/user/domain/repositories/user.repository';
+import { UserRepository } from '@src/user/domain/repositories/userRepository.interface';
 import { UserValue } from '@user/domain/user.value';
 import { UserServiceInterface } from '@user/application/ports/user.interface';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UserService implements UserServiceInterface {
-  // constructor(private userReposirory: UserRepository) {}
+  constructor(private userReposirory: UserRepository) {}
 
-  async findUserById(id: string): Promise<string> {
-    console.log('Get User...' + id);
-    return `HELLO WORLD id: ${id}`;
+  async findUserById(publicId: string): Promise<UserValue> {
+    console.log('Get User...' + publicId);
+    const user: UserValue = await this.userReposirory.findUserByPublicId(
+      publicId,
+    );
+    console.log(user);
+    return user;
   }
   async createUser(user: UserValue): Promise<UserValue> {
+    user = { ...user, publicId: uuidv4().split('-')[0] };
     const newUser: UserValue = new UserValue(user);
-    console.log(newUser);
-    return newUser;
+    const userCreated = await this.userReposirory.createUser(newUser);
+    console.log('User: ', userCreated);
+    return userCreated;
   }
 }
