6b3c0b2ce9c4b7832cb86c10c9a7f89aa23ac7ab
andres06-hub <andressierrarojas7@gmail.com>
2023-03-23 19:33:29 -0500

    CREATED: method post by create user

--- src/user/infrastructure/controllers/user.controller.ts
+++ src/user/infrastructure/controllers/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { UserValue } from '@src/user/domain/user.value';
 import { UserServiceInterface } from '@user/application/ports/user.interface';
 
 @Controller('user')
@@ -11,5 +12,10 @@ export class UserController {
   @Get(':id')
   async getUser(@Param('id') id: string) {
     return this._userSrv.findUserById(id);
+  }
+
+  @Post()
+  async createUser(@Body() user: UserValue) {
+    return this._userSrv.createUser(user);
   }
 }
