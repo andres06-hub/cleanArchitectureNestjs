d0067012193998ffcaa25403abdef19f9fb77d3c
andres06-hub <andressierrarojas7@gmail.com>
2023-03-27 18:00:20 -0500

    refactor: character

--- src/auth/domain/repositories/userRepository.interface.ts
+++ src/auth/domain/repositories/userRepository.interface.ts
@@ -1,5 +1,5 @@
 import { UserModel } from '@auth/domain/models/auth.model';
 
 export interface AuthRepository {
-  finduserByEmail(email: string): Promise<UserModel>;
+  findUserByEmail(email: string): Promise<UserModel>;
 }
