44df3f68e0da96deb32c539fdbdeac2966ff8504
andres06-hub <andressierrarojas7@gmail.com>
2023-03-27 18:12:53 -0500

    feat: created controller v1

---
+++ src/auth/infrastructure/controllers/auth.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  AUTH_PORT_SERVICE,
+  AuthServiceInterface,
+} from '@auth/application/ports/auth.port';
+import { Response } from '@auth/domain/dto/response.dto';
+import { LoginDto } from '@auth/domain/dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    @Inject(AUTH_PORT_SERVICE)
+    private readonly authSrv: AuthServiceInterface,
+  ) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UsePipes(ValidationPipe)
+  async login(@Body() data: LoginDto): Promise<HttpException | Response> {
+    const result: HttpException | Response = await this.authSrv.login(data);
+    return result;
+  }
+}
