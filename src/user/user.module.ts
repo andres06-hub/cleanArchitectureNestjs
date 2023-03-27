24625820f1b09fe163143645f82450b52a937b7c
andres06-hub <andressierrarojas7@gmail.com>
2023-03-26 23:20:02 -0500

    refactor(provider): added service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,12 +5,18 @@ import {
   UserSrvIntProvider,
 } from '@src/user/infrastructure/providers/user.provider';
 import { DatabaseModule } from '@providers/database/database.module';
-import { DatabaseUserRepository } from './infrastructure/repositories/user.repository';
+import { DatabaseUserRepository } from '@user/infrastructure/repositories/user.repository';
+import { EncryptionService } from './application/usecases/encryption/encryption.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserDbProvider, DatabaseUserRepository, UserSrvIntProvider],
+  providers: [
+    UserDbProvider,
+    DatabaseUserRepository,
+    EncryptionService,
+    UserSrvIntProvider,
+  ],
   exports: [],
 })
 export class UserModule {}
