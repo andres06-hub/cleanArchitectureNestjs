3c948e4f07e4d3a36772695e4fd5f1a8e4673903
andres06-hub <andressierrarojas7@gmail.com>
2023-03-27 18:16:29 -0500

    feat(v1): auth module created, there will be a new version, since there is an inconsistency

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@database/database.module';
+import { AuthController } from '@auth/infrastructure/controllers/auth.controller';
+import {
+  AuthSrvProvider,
+  UserDbProvider,
+} from '@auth/infrastructure/providers/auth.providers';
+import { DatabaseUserRepository } from '@auth/infrastructure/repositories/auth.repository';
+import { EncryptionService } from '@auth/application/usecases/encryption/encryption.service';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+  controllers: [AuthController],
+  providers: [
+    UserDbProvider,
+    DatabaseUserRepository,
+    AuthSrvProvider,
+    EncryptionService,
+  ],
+})
 export class AuthModule {}
