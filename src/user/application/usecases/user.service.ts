8fd18cdfd4bdc748f2c4a85475cc87924a7e2ae0
andres06-hub <andressierrarojas7@gmail.com>
2023-03-26 23:07:41 -0500

    feat(encryption): implementation

--- src/user/application/usecases/user.service.ts
+++ src/user/application/usecases/user.service.ts
@@ -1,20 +1,26 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
 import { UserRepository } from '@user/domain/repositories/userRepository.interface';
 import { UserValue } from '@user/domain/user.value';
 import { UserServiceInterface } from '@user/application/ports/user.interface';
 import { v4 as uuidv4 } from 'uuid';
 import { UserModel } from '@user/domain/models/user.model';
 import { Response } from '@user/domain/dto/response.dto';
+import { Encryption } from '@src/user/domain/interfaces/encryption.interface';
 
 @Injectable()
 export class UserService implements UserServiceInterface {
-  constructor(private userReposirory: UserRepository) {}
+  constructor(
+    private readonly userReposirory: UserRepository,
+    private readonly encryption: Encryption,
+  ) {}
+
+  private log: Logger = new Logger();
 
   async findUserById(publicId: string): Promise<Response> {
     const user: UserValue = await this.userReposirory.findUserByPublicId(
       publicId,
     );
-    console.log(user);
+    this.log.log('User found');
     return new Response(true, 'User found!', user);
   }
   async createUser(user: UserModel): Promise<Response> {
@@ -22,10 +28,11 @@ export class UserService implements UserServiceInterface {
       await this.userReposirory.findUserByEmail(user.email);
     if (findUser)
       throw new HttpException(new Response(true, 'User Exists!'), 200);
-    user = { ...user, publicId: uuidv4().split('-')[0] };
+    const pwdhash: string = await this.encryption.encrypt(user.password);
+    user = { ...user, publicId: uuidv4().split('-')[0], password: pwdhash };
     const newUser: UserValue = new UserValue(user);
     const userCreated = await this.userReposirory.createUser(newUser);
-    console.log('User: ', userCreated);
+    this.log.log('User created');
     return new Response(true, 'User Created!', userCreated);
   }
 }
