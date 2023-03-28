ebdcb45b039b1a6261bb86f49a30166f316adf51
andres06-hub <andressierrarojas7@gmail.com>
2023-03-28 01:56:51 -0500

    refactor: function and injector, function validate token created

--- src/auth/application/usecases/auth.service.ts
+++ src/auth/application/usecases/auth.service.ts
@@ -1,21 +1,24 @@
-import {
-  ForbiddenException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
 import { AuthServiceInterface } from '@auth/application/ports/auth.port';
-import { Response } from '@src/auth/domain/dto/response.dto';
-import { LoginModel } from '@src/auth/domain/models/auth.model';
-import { AuthRepository } from '@src/auth/domain/repositories/userRepository.interface';
+import { Response } from '@auth/domain/dto/response.dto';
+import { LoginModel } from '@auth/domain/models/auth.model';
+import { AuthRepository } from '@auth/domain/repositories/userRepository.interface';
 import { Encryption } from '@auth/domain/interfaces/encryption.interface';
 import { UserModel } from '@auth/domain/models/auth.model';
+import {
+  JwtPayload,
+  TokenSrvInterface,
+} from '@src/auth/domain/interfaces/jwt.interface';
 
 @Injectable()
 export class AuthService implements AuthServiceInterface {
   constructor(
     private readonly authRpt: AuthRepository,
     private readonly encryption: Encryption,
+    private readonly tokenSrv: TokenSrvInterface,
   ) {}
+
+  private log: Logger = new Logger();
 
   async login(data: LoginModel): Promise<Response> {
     const findUser: UserModel | null = await this.authRpt.findUserByEmail(
@@ -36,11 +39,18 @@ export class AuthService implements AuthServiceInterface {
         new Response(false, 'Incorrect credentials'),
       );
     // Create Token
-    // const token: string = this._jwtSrv.sign({
-    //   publicId: findUser.publicId,
-    //   name: findUser.name,
-    // });
+    const token: string = await this.tokenSrv.createToken<JwtPayload>({
+      publicId: findUser.email,
+      name: findUser.name,
+    });
+    this.log.log('Successfully log-in', new Date(), AuthService.name);
     //Return JWT
-    return new Response(true, 'Successfully logged in!');
+    return new Response(true, 'Successfully log-in!', { token });
+  }
+
+  async isValidToken(token: string): Promise<Response> {
+    await this.tokenSrv.checkToken(token);
+    this.log.log('Token valid', new Date(), AuthService.name);
+    return new Response(true, 'Valid Token');
   }
 }
