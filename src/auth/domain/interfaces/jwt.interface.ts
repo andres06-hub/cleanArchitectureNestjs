e45c87de3e3923f0b0dda4030ce8e341707370e2
andres06-hub <andressierrarojas7@gmail.com>
2023-03-28 01:52:16 -0500

    feat(interface): created for token service

--- src/auth/domain/interfaces/jwt.interface.ts
+++ src/auth/domain/interfaces/jwt.interface.ts
@@ -2,4 +2,11 @@ export interface JwtPayload {
   //Data going into the JWT payload
   publicId: string;
   name: string;
+  iat?: number;
+  exp?: number;
+}
+
+export interface TokenSrvInterface {
+  createToken<T>(data: T): Promise<string>;
+  checkToken(token: string): Promise<boolean>;
 }
