f044bc1c5f9af29bd4b2dbe2267d485822188f54
andres06-hub <andressierrarojas7@gmail.com>
2023-03-28 01:57:43 -0500

    refactor: provider

--- src/auth/infrastructure/providers/auth.providers.ts
+++ src/auth/infrastructure/providers/auth.providers.ts
@@ -4,6 +4,8 @@ import { AuthService } from '@auth/application/usecases/auth.service';
 import { DatabaseUserRepository } from '@auth/infrastructure/repositories/auth.repository';
 import { AUTH_PORT_SERVICE } from '@auth/application/ports/auth.port';
 import { EncryptionService } from '@auth/application/usecases/encryption/encryption.service';
+import { JwtService } from '@nestjs/jwt';
+import { TokenService } from '@src/auth/application/usecases/token/token.service';
 
 export const UserDbProvider = {
   provide: 'USER_CONNECTION',
@@ -12,10 +14,11 @@ export const UserDbProvider = {
 };
 
 export const AuthSrvProvider = {
-  inject: [DatabaseUserRepository, EncryptionService],
+  inject: [DatabaseUserRepository, EncryptionService, JwtService],
   provide: AUTH_PORT_SERVICE,
   useFactory: (
     dbUserRepository: DatabaseUserRepository,
     encryption: EncryptionService,
-  ) => new AuthService(dbUserRepository, encryption),
+    jwtSrv: JwtService,
+  ) => new AuthService(dbUserRepository, encryption, new TokenService(jwtSrv)),
 };
