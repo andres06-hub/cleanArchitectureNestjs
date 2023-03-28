b685b27c79743564253456bd80fca8fed156684f
andres06-hub <andressierrarojas7@gmail.com>
2023-03-28 02:00:09 -0500

    refactor(add): jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,9 +7,20 @@ import {
 } from '@auth/infrastructure/providers/auth.providers';
 import { DatabaseUserRepository } from '@auth/infrastructure/repositories/auth.repository';
 import { EncryptionService } from '@auth/application/usecases/encryption/encryption.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.registerAsync({
+      useFactory: () => {
+        return {
+          secret: 'example-secret',
+          signOptions: { expiresIn: '60s' },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [
     UserDbProvider,
