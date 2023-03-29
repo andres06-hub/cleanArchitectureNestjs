b4f4aadf0f647278c9c327e72aca6c827bbe1614
andres06-hub <andressierrarojas7@gmail.com>
2023-03-29 15:30:42 -0500

    build(lint): add extends prettier, and ignore folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'eslint-config-prettier'
+    // 'plugin:prettier/recommended',
   ],
   root: true,
   env: {
