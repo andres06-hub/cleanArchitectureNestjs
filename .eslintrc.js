1105163f3b09ffdfa33f36ddd7c82ed7014a4b16
andres06-hub <andressierrarojas7@gmail.com>
2023-03-29 21:23:35 -0500

    build: format

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'eslint-config-prettier'
-    // 'plugin:prettier/recommended',
+    // 'eslint-config-prettier'
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
